d74f5a689062cdcc53efe127cbb3d273e1dc0dcb
Nixis198 <gager198@gmail.com>
2020-01-18 17:29:11 -0500

    Added auto

--- src/main/java/frc/robot/commands/DriveTime.java
+++ src/main/java/frc/robot/commands/DriveTime.java
@@ -7,17 +7,15 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class DriveTime extends CommandBase {
   private final DriveSubsystem m_drive;
   private final double m_time;
   private final double m_speed;
-  private WaitCommand waitCommand;
-  private boolean isDone;
-
+  private final Timer m_timer = new Timer();
   /**
    * Creates a new DriveTime.
    */
@@ -31,31 +29,27 @@ public class DriveTime extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_drive.arcadeDrive(m_speed, 0);
-    isDone = false;
-    waitCommand = new WaitCommand(m_time);
-    waitCommand.schedule();
+    m_drive.stopDrive();
+    m_timer.reset();
+    m_timer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(waitCommand.isFinished()){
-      isDone = true;
-    }else{
-      isDone = false;
-    }
+    m_drive.arcadeDrive(m_speed, 0);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_drive.arcadeDrive(0, 0);
+    m_drive.stopDrive();
+    m_timer.stop();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return isDone;
+    return m_timer.hasPeriodPassed(m_time);
   }
 }
